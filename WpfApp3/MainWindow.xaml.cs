4142657644d22f8509b9173e3f4f16db0a70a8e8
Misha Odozhiy <misha.odozhiy.04@mail.ru>
2022-11-30 14:24:01 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static string LoadPage(string url)
+        {
+            var result = "";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var receiveStream = response.GetResponseStream();
+                if (receiveStream != null)
+                {
+                    StreamReader readStream;
+                    if (response.CharacterSet == null)
+                        readStream = new StreamReader(receiveStream);
+                    else
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    result = readStream.ReadToEnd();
+                    readStream.Close();
+                }
+                response.Close();
+            }
+            return result;
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string pageContent = LoadPage(url: @"https://time100.ru/");
+            var document = new HtmlDocument();
+            document.LoadHtml(pageContent);
+
+
+            HtmlNodeCollection links = document.DocumentNode.SelectNodes("/html/body/header/h3[1]/span");
+            name.Text = links.Last().InnerText;
+        }
+    }
+}
